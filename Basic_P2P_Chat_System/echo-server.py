73ebcc70f115ce582213b0360e3d7530c10a8850
trevorpoppe <tpoppe@bu.edu>
2025-03-25 18:16:27 -0400

    Update echo-server.py

--- Basic_P2P_Chat_System/echo-server.py
+++ Basic_P2P_Chat_System/echo-server.py
@@ -5,10 +5,11 @@ import socket
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
+# Create a socket object
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
+    s.bind((HOST, PORT)) # Bind the server to the specified host and port
+    s.listen(1) # Listen for incoming connections (max 1 connection)
+    conn, addr = s.accept() # Accept the incoming connection
     with conn:
         print(f"Connected by {addr}")
         while True:
