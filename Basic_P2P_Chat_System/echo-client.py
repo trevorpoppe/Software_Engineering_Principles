6e706d696f9aae57c782f9c825cb07867c9e8084
trevorpoppe <tpoppe@bu.edu>
2025-03-25 18:18:24 -0400

    Update echo-client.py

--- Basic_P2P_Chat_System/echo-client.py
+++ Basic_P2P_Chat_System/echo-client.py
@@ -3,9 +3,10 @@ import socket
 HOST = '127.0.0.1'  # Localhost
 PORT = 65432        # Port to listen on
 
+# Create a socket object
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
+    s.bind((HOST, PORT)) # Bind the client to the host and port
+    s.listen(1) # Listen to incoming connections (max 1)
     print(f"Server listening on {HOST}:{PORT}")
     
     conn, addr = s.accept()  # Accept a connection
